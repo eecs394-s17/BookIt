30d24808c9d5d59722b78268e8409a826e3fbde0
Lily Zhang <lilyszhang96@gmail.com>
2017-04-03 21:31:50 -0500

    fixed bug

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { DetailPage } from '../pages/detail/detail';
-import { AddChorePage } from '../pages/add_chore/add_chore';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +18,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     DetailPage,
-    AddChorePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -32,7 +30,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     DetailPage,
-    AddChorePage
   ],
   providers: [
     StatusBar,
