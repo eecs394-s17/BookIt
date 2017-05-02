4912a342b05e34f14630b6e8e5086d5956c936a6
spenserhuang <spenserhuang515@gmail.com>
2017-05-02 07:33:24 -0500

    Final UI changes

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,22 +1,54 @@
 declare var loggedIn: number;
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import {TaskService} from '../../providers/task-service';
+
 import { LoginPage } from '../login/login';
 import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
+  providers: [TaskService]
 })
+
 export class HomePage {
+  items = [];
 
   @ViewChild('barCanvas') barCanvas;
 
   barChart: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private taskService: TaskService) {
 
-  }
+  };
+
+  ionViewNumTasks() {
+    var lilyCount = 0;
+    var sehmonCount = 0;
+    var spenserCount = 0;
+    var johnCount = 0;
+    var tasksData = [lilyCount, sehmonCount, spenserCount, johnCount];
+
+      this.taskService.grabTasks().subscribe(
+        data => {
+          this.items = [];
+          this.items = data;
+          console.log(data);
+          for (var i=0; i < data.length; i++) {
+
+          }
+        }
+      );
+      err => {
+          console.log(err);
+      }
+      () => console.log("Task Grab Complete")
+  };
+
+
 
   ionViewDidLoad() {
 
@@ -27,7 +59,7 @@ export class HomePage {
                 labels: ["Sehmon", "Spenser", "John", "Lily"],
                 datasets: [{
                     label: 'Total',
-                    data: [12, 19, 3, 5],
+                    data: [6, 2, 4, 8],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
